56a3c307876e39f68041360e7785fc02b50fc8ed
Johannes <johanneskr@edu.aau.at>
2025-04-04 15:43:28 +0200

    Removed unnecessary class files, added comments, added sample methods for lobby

--- src/main/java/at/aau/serg/websocketdemoserver/websocket/broker/WebSocketBrokerConfig.java
+++ src/main/java/at/aau/serg/websocketdemoserver/websocket/broker/WebSocketBrokerConfig.java
@@ -12,14 +12,16 @@ public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic");    //Server broadcasts messages to this destination (send)
+        config.setApplicationDestinationPrefixes("/app");        //Clients send messages to this destination (received)
+        //config.setUserDestinationPrefix("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket-example-broker")
+        registry.addEndpoint("/cluedo")
                 .setAllowedOrigins("*");
+                //.withSockJS(); try if websockets are not supported.
     }
 }
 
--- src/main/java/at/aau/serg/websocketdemoserver/websocket/broker/WebSocketBrokerController.java
+++ src/main/java/at/aau/serg/websocketdemoserver/websocket/broker/WebSocketBrokerController.java
@@ -9,12 +9,30 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class WebSocketBrokerController {
+
+    @MessageMapping("/login")
+    @SendTo("/topic/login-response")
+    public String handleLogin(String username) {
+        // TODO handle login here
+        return "";
+    }
+
+    @MessageMapping("/disconnect")
+    @SendTo("/topic/disconnect-response")
+    public String handleDisconnect(String username) {
+        // TODO handle disconnect here
+        return "";
+    }
+
+
+    //sample methods for exchange of String-messages or StompMessages / Json Objects
     @MessageMapping("/hello")
     @SendTo("/topic/hello-response")
     public String handleHello(String text) {
         // TODO handle the messages here
         return "echo from broker: "+text;
     }
+
     @MessageMapping("/object")
     @SendTo("/topic/rcv-object")
     public StompMessage handleObject(StompMessage msg) {
