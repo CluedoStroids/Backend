4d7f56ac0a95f0276cd65ba1913178d9c24bed04
Kilian Smrcka <151249357+kilian-smrcka@users.noreply.github.com>
2025-04-05 12:49:10 +0200

    changed name of main-file

--- src/main/java/at/aau/se2/cluedo/CluedoApplication.java
+++ src/main/java/at/aau/se2/cluedo/CluedoApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class WebSocketDemoServerApplication {
+public class CluedoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebSocketDemoServerApplication.class, args);
+        SpringApplication.run(CluedoApplication.class, args);
     }
 
 }
